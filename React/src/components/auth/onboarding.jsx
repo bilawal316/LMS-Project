1c8d2d8e7028115fa5884cf5f5974ff027628c5e
bilawal316 <mbilawalzaman@gmail.com>
2023-12-29 17:29:49 +0500

    onboarding update

--- React/src/components/auth/onboarding.jsx
+++ React/src/components/auth/onboarding.jsx
@@ -3,34 +3,44 @@ import axios from 'axios';
 import Select from "react-select";
 import { useLocation } from "react-router-dom";
 
-
 const Onboarding = (updateState) => {
   const [instructors, setInstructors] = useState("");
   const [instructor, setInstructor] = useState([]);
+  const [requestSent, setRequestSent] = useState(false);
   const location = useLocation();
-  const userId = location.state.userId
+  const userId = location.state.userId;
 
   const getAllInstructors = async () => {
     await axios.get("http://localhost:3000/user/getAllInstructor").then((res) => {
-      console.log(res)
-      const list_arr = res.data.response.map(elem => (
-        {
-          value: elem.userId,
-          label : [elem.firstName,elem.lastName].join(" ")
-        } 
-      ));
+      console.log(res);
+      const list_arr = res.data.response.map(elem => ({
+        value: elem.userId,
+        label: [elem.firstName, elem.lastName].join(" ")
+      }));
       setInstructors(list_arr);
-    })
-    
+    });
   };
+
   const onboarding = async (e) => {
     e.preventDefault();
+
+    // Check if the request has already been sent
+    if (requestSent) {
+      alert("Your request has already been sent. Please wait for approval.");
+      return;
+    }
+
     const { data } = await axios.post("http://localhost:3000/user/onbaording", {
       instructorId: instructor,
       userId
     });
     console.log("data ", data);
-}
+
+    // Update state to indicate that the request has been sent
+    setRequestSent(true);
+
+    alert("Your request has been sent. Please wait for approval.");
+  };
 
   useEffect(() => {
     getAllInstructors();
@@ -49,32 +59,33 @@ const Onboarding = (updateState) => {
               Onboarding
             </h1>
             <form className="mt-6">
-              <div className="mb-2">
-                <label
-                  className="block text-sm font-semibold text-gray-800"
-                >
-                  Select Instructor
-                </label>
-                <Select
-              className="bg-white  rounded-lg mb-2 focus:outline-none text-black font-medium"
-              isSearchable={true}
-              options={instructors}
-              onChange={(e) => {
-                setInstructor(e.value);
-              }}
-              isDisabled={false}
-              placeholder="Select Role"
-            />
-              </div>
-              <div className="mt-6">
+            <div className="mb-2">
+              <label className="block text-sm font-semibold text-gray-800">
+                Select Instructor
+              </label>
+              <Select
+                className="bg-white  rounded-lg mb-2 focus:outline-none text-black font-medium"
+                isSearchable={true}
+                options={instructors}
+                onChange={(e) => {
+                  setInstructor(e.value);
+                }}
+                isDisabled={false}
+                placeholder="Select Role"
+              />
+            </div>
+            <div className="mt-6">
               <button
-                    onClick={(e) => { onboarding(e) }}
-                    className="w-full px-4 py-2 tracking-wide bg-purple-700 text-white
-                    transition-colors duration-200 transform rounded-md focus:outline-none">
-                    Select Instructor
-            </button>
-              </div>
-            </form>
+                onClick={(e) => {
+                  onboarding(e);
+                }}
+                className="w-full px-4 py-2 tracking-wide bg-purple-700 text-white
+                    transition-colors duration-200 transform rounded-md focus:outline-none"
+              >
+                Select Instructor
+              </button>
+            </div>
+          </form>
           </div>
         </div>
       </div>
